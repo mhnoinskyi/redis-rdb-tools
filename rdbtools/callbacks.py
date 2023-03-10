7f5533de165c3cb488eab6538cb4f88d1c7f2eb2
Maksym Hnoinskyi <m.hnoinskyi@geniusee.com>
2023-03-10 17:37:22 +0200

    fix typo

--- rdbtools/callbacks.py
+++ rdbtools/callbacks.py
@@ -189,7 +189,7 @@ class JSONRichCallback(RdbCallback):
 
     def set(self, key, value, expiry, info):
         self._start_key(key, 0)
-        self._out.write(b'{"key":' + self.encode_key(key),
+        self._out.write(b'{"key":' + self.encode_key(key)
                         + b','
                         + (b'"expireat":' + self.encode_value(expiry) + b',' if expiry else b'')
                         + b'"type":"string","value":'
